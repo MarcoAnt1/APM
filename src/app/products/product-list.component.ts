f5ad61dbb1757b7d358b270da34a8ddb81a9b4ea
Marco Antonio de Souza Filho <54957584+MarcoAnt1@users.noreply.github.com>
2021-10-29 20:29:55 -0300

    Property Binding.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,7 +5,9 @@ import { Component } from "@angular/core";
     templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
-    pageTitle: string = 'Procudt List!';
+    pageTitle = 'Procudt List!';
+    imageWidth = 50;
+    imageMargin = 2;
     products: any[] = [
         {
             "productId": 2,
